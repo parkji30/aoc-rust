684fb32888888a14b7342e6e0dc4ed1a5029e4fb
james.park <james.park@xanadu.ai>
2025-09-28 10:22:28 -0400

    historian hysteria day 1 solution

--- aoc/aoc-2024/src/day1.rs
+++ aoc/aoc-2024/src/day1.rs
@@ -1,13 +1,13 @@
-pub fn historian_hysteria() -> Vec<i32> {
+pub fn historian_hysteria() -> i32 {
     let mut list1: [i32; 6] = [3, 4, 2, 1, 3, 3];
     let mut list2: [i32; 6] = [4, 3, 5, 3, 9, 3];
-
+    let mut net_distace: i32 = 0;
+    
     list1.sort();
     list2.sort();
-
-    let mut distance_diff: Vec<i32> = Vec::new();
+ 
     for (value1, value2) in list1.iter().zip(list2.iter()){
-        distance_diff.push(value1 - value2)
+        net_distace += (value1 - value2).abs()
     }
-    distance_diff
+    net_distace
 }
