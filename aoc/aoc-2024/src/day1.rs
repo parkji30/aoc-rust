e8827341fa972401a6100a23748694b0911e9841
james.park <james.park@xanadu.ai>
2025-09-30 08:21:38 -0400

    spelling error

--- aoc/aoc-2024/src/day1.rs
+++ aoc/aoc-2024/src/day1.rs
@@ -1,13 +1,14 @@
 pub fn historian_hysteria() -> i32 {
     let mut list1: [i32; 6] = [3, 4, 2, 1, 3, 3];
     let mut list2: [i32; 6] = [4, 3, 5, 3, 9, 3];
-    let mut net_distace: i32 = 0;
+    let mut distance: i32 = 0;
     
     list1.sort();
     list2.sort();
  
     for (value1, value2) in list1.iter().zip(list2.iter()){
-        net_distace += (value1 - value2).abs()
+        min_distance += (value1 - value2).abs()
     }
-    net_distace
+    min_distance
+
 }
