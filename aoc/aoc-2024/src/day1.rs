921af126b3e1d72f0bc179dc91e36fe7042c79d4
james.park <james.park@xanadu.ai>
2025-10-01 17:50:17 -0400

    update day1 to work as intended

--- aoc/aoc-2024/src/day1.rs
+++ aoc/aoc-2024/src/day1.rs
@@ -1,14 +1,38 @@
-pub fn historian_hysteria() -> i32 {
-    let mut list1: [i32; 6] = [3, 4, 2, 1, 3, 3];
-    let mut list2: [i32; 6] = [4, 3, 5, 3, 9, 3];
-    let mut distance: i32 = 0;
-    
-    list1.sort();
-    list2.sort();
- 
-    for (value1, value2) in list1.iter().zip(list2.iter()){
-        min_distance += (value1 - value2).abs()
-    }
-    min_distance
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
 
+
+pub fn historian_hysteria() -> Result<i32, Error> {
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let data_path = current_dir.join("data").join("day1.txt");
+    
+    let file = File::open(data_path).expect("Failed to open path");
+    let reader = BufReader::new(file);
+    
+    let mut first_journey: Vec<i32> = Vec::new();
+    let mut second_journey: Vec<i32> = Vec::new();
+
+    for line in reader.lines(){
+        match line {
+            Ok(content) => {
+                let parts: Vec<&str> = content.split_whitespace().collect();
+                let left: i32 = parts[0].parse().expect("Failed to get left");
+                let right: i32 = parts[1].parse().expect("Failed to get right");
+                first_journey.push(left);
+                second_journey.push(right);                
+            },
+            Err(err_message) => eprintln!("Error reading line: {}", err_message)
+        }
+    }
+
+    let mut total_distance: i32 = 0;
+    
+    first_journey.sort();
+    second_journey.sort();
+
+    for (first_value, second_value) in first_journey.iter().zip(second_journey.iter()){
+        total_distance += (first_value - second_value).abs();
+    }
+    Ok(total_distance)
 }
--- aoc/aoc-2024/src/main.rs
+++ aoc/aoc-2024/src/main.rs
@@ -3,8 +3,11 @@ use day1::historian_hysteria;
 
 fn main() {
     println!("Day 1 of Advent of Code (Rust)");
-    let distance_diff = historian_hysteria();
-    println!("{:?}", distance_diff);
+
+    match historian_hysteria() {
+        Ok(result) => println!("{}", result),
+        Err(err_message) => println!("Error: {}", err_message)
+    }
     
     println!("Advent of Code 2024 - Rust Solutions");
 }
